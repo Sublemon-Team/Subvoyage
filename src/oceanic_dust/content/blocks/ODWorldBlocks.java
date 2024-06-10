4d200a966ce8895de498da3fcc683bf9155b1838
IriDark <idarkmage228@gmail.com>
2024-06-10 15:09:41 +0300

    я устал

--- src/oceanic_dust/content/blocks/ODWorldBlocks.java
+++ src/oceanic_dust/content/blocks/ODWorldBlocks.java
@@ -1,13 +1,20 @@
 package oceanic_dust.content.blocks;
 
 import mindustry.world.*;
+import mindustry.world.blocks.defense.*;
 import mindustry.world.blocks.environment.*;
+import mindustry.world.meta.*;
 import oceanic_dust.content.world.items.*;
 
 public class ODWorldBlocks {
     public static Block
-        //ores
-        orePhosphorus,oreCorallite,oreSulfur,oreIridium;
+    //ores
+    orePhosphorus,oreCorallite,oreSulfur,oreIridium,
+    // floors
+    legartyteStone,
+    // walls
+    legartyteWall;
+
 
     public static void load() {
         orePhosphorus = new OreBlock(ODItems.spaclanium){{
@@ -29,6 +36,14 @@ public class ODWorldBlocks {
             oreDefault = false;
             oreThreshold = 0.41f;
             oreScale = 12.42614f;
+        }};
+        legartyteStone = new Floor("legartyte-stone"){{
+            attributes.set(Attribute.water, -1f);
+            variants = 3;
+            wall = legartyteWall;
+        }};
+        legartyteWall = new StaticWall("legartyte-wall"){{
+            variants = 3;
         }};
     }
 }
