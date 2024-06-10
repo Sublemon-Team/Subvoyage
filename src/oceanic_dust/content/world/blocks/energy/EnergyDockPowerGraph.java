cb361b5f2ca3b6c26298e3ad4a0e3729cea10aac
Skoow <89569841+VuzZis@users.noreply.github.com>
2024-06-10 21:15:59 +0500

    also

---
+++ src/oceanic_dust/content/world/blocks/energy/EnergyDockPowerGraph.java
@@ -0,0 +1,10 @@
+package oceanic_dust.content.world.blocks.energy;
+
+import mindustry.world.blocks.power.PowerGraph;
+
+public class EnergyDockPowerGraph extends PowerGraph {
+    @Override
+    public void transferPower(float amount) {
+        super.transferPower(amount);
+    }
+}
--- src/oceanic_dust/content/world/blocks/energy/EnergyDockPowerModule.java
+++ src/oceanic_dust/content/world/blocks/energy/EnergyDockPowerModule.java
@@ -1,7 +1,11 @@
 package oceanic_dust.content.world.blocks.energy;
 
+import mindustry.world.blocks.power.PowerGraph;
 import mindustry.world.modules.PowerModule;
 
 public class EnergyDockPowerModule extends PowerModule {
-    
+    public PowerGraph graph = new EnergyDockPowerGraph();
+    public EnergyDockPowerModule() {
+        super.graph = this.graph;
+    }
 }
