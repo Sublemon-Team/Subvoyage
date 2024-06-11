ec4c374a5d0b4283da9b9c0d7ca59b75733e0621
Skoow <89569841+VuzZis@users.noreply.github.com>
2024-06-11 11:10:03 +0500

    generation tweaks

--- src/oceanic_dust/content/world/planets/AtlacianPlanetGenerator.java
+++ src/oceanic_dust/content/world/planets/AtlacianPlanetGenerator.java
@@ -3,6 +3,8 @@ package oceanic_dust.content.world.planets;
 import arc.graphics.*;
 import arc.math.*;
 import arc.math.geom.*;
+import arc.struct.FloatSeq;
+import arc.struct.Seq;
 import arc.util.*;
 import arc.util.noise.*;
 import mindustry.ai.*;
@@ -12,21 +14,45 @@ import mindustry.maps.generators.*;
 import mindustry.type.*;
 import mindustry.type.Weather.*;
 import mindustry.world.*;
+import mindustry.world.blocks.environment.OreBlock;
 
 import static mindustry.Vars.*;
 import static oceanic_dust.content.blocks.ODWorldBlocks.*;
 
 public class AtlacianPlanetGenerator extends PlanetGenerator {
     float ocean = -0.4f;
-    public static float liqThresh = 0.3f, liqScl = 240f;
+    public static float liqThresh = 0.2f, liqScl = 60f;
     Block[][] arr = {
-    {Blocks.carbonStone, legartyteStone, agaryteStone, Blocks.carbonStone, legartyteStone, Blocks.carbonStone, agaryteStone, Blocks.carbonStone, Blocks.carbonStone}
+    {Blocks.carbonStone, legartyteStone, agaryteStone,Blocks.carbonStone, legartyteStone, Blocks.carbonStone,
+            agaryteStone, Blocks.carbonStone, Blocks.carbonStone,Blocks.carbonStone, legartyteStone,
+            agaryteStone,Blocks.carbonStone,Blocks.carbonStone,legartyteStone, agaryteStone}
     };
 
     {
         defaultLoadout = ODLoadouts.basicPuffer;
         baseSeed = 1;
     }
+
+    private Seq<Block> getOres() {
+        Seq<Block> ores = Seq.with(oreSpaclanium,oreCorallite);
+        float poles = Math.abs(sector.tile.v.y);
+        float nmag = 0.5f;
+        float scl = 1f;
+        float addscl = 1.3f;
+
+        if(Simplex.noise3d(seed, 2, 0.5, scl, sector.tile.v.x, sector.tile.v.y, sector.tile.v.z)*nmag + poles > 0.25f*addscl){
+            ores.add(oreSulfur);
+        }
+
+        if(Simplex.noise3d(seed, 2, 0.5, scl, sector.tile.v.x + 1, sector.tile.v.y, sector.tile.v.z)*nmag + poles > 0.52f*addscl){
+            ores.add(oreIridium);
+        }
+
+        /*if(Simplex.noise3d(seed, 2, 0.5, scl, sector.tile.v.x + 2, sector.tile.v.y, sector.tile.v.z)*nmag + poles > 0.7f*addscl){
+            ores.add(Blocks.oreThorium);
+        }*/
+        return ores;
+    };
 
     @Override
     public void generateSector(Sector sector){
@@ -83,6 +109,8 @@ public class AtlacianPlanetGenerator extends PlanetGenerator {
         if(tile.floor == Blocks.bluemat && rand.chance(0.4)){
             tile.block = Blocks.yellowCoral;
         }
+
+        if(tile.block == Blocks.sandWall) tile.block = legartyteWall;
     }
 
     @Override
@@ -90,13 +118,20 @@ public class AtlacianPlanetGenerator extends PlanetGenerator {
         median(2, 0.6);
         cells(4);
         pass((x, y) -> {
-            if(floor == Blocks.beryllicStone && noise(x, y, 3, 0.4f, 8f, 0.8f) > 0.45f){
-                block = Blocks.beryllicStoneWall;
-            }
+            if(floor == legartyteStone && noise(x, y, 3, 0.4f, 12f, 0.8f) > 0.35f)
+                block = legartyteWall;
 
-            if(floor == Blocks.carbonStone && noise(x, y, 6, 0.2f, 12f, 0.8f) > 0.45f){
+            if(floor == Blocks.carbonStone && noise(x, y, 6, 0.5f, 16f, 0.8f) > 0.35f)
                 block = Blocks.carbonWall;
-            }
+
+            if(floor == Blocks.carbonStone && noise(x, y, 4, 0.5f, 13f, 0.8f) > 0.35f)
+                block = noise(x, y, 2, 0.5f, 16f, 0.8f) > 0.2f ? agaryteWall : legartyteWall;
+
+            if(floor == Blocks.carbonWall && noise(x, y, 2, 0.5f, 16f, 0.8f) > 0.4f)
+                block = noise(x, y, 2, 0.5f, 16f, 0.8f) > 0.2f ? agaryteWall : legartyteWall;
+
+            if(block == Blocks.sandWall)
+                block = legartyteWall;
 
             float max = 0;
             for(Point2 p : Geometry.d8){
@@ -109,39 +144,101 @@ public class AtlacianPlanetGenerator extends PlanetGenerator {
 
             if(floor != Blocks.carbonStone) return;
             if(nearWall(x, y)) return;
-            float noise = noise(-x + 300, -y -x*1.6f + 100, 2, 0.8f, liqScl/2, 2f);
-            if(noise > liqThresh){
+            float noise = noise(-y + 300, -x + 100, 2, 0.5f, liqScl, 0.3f);
+            float secondNoise = noise(y + 300, x - 100, 4, 0.5f, liqScl/2, 0.4f);
+            float thirdNoise = noise(x,y,2,0.3f,10f,0.12f);
+            float fourthNoise = Simplex.noise3d(seed,4,0.3,25,x,y,sector.id*10);
+            if(noise > liqThresh || secondNoise < liqThresh*2){
+                floor = Blocks.water;
+            }
+            if(thirdNoise > liqThresh/2) {
+                block = Blocks.water;
+                floor = Blocks.water;
+            }
+            if(fourthNoise > liqThresh/2) {
                 floor = Blocks.water;
             }
         });
 
-        median(3, 0.6);
-        distort(5f, 8f);
-        blend(Blocks.water, Blocks.darksandWater, 4);
+        pass((x, y) -> {
+            if(block.solid) return;
+
+            Vec3 v = sector.rect.project(x, y);
+
+            float rr = Simplex.noise2d(sector.id, (float)2, 0.6f, 1f / 7f, x, y) * 0.1f;
+            float value = Ridged.noise3d(2, v.x, v.y, v.z, 1, 1f / 55f) + rr - rawHeight(v) * 0f;
+            float rrscl = rr * 44 - 2;
+            if(value < 0.17f) return;
+            //do not place rivers on ice, they're frozen
+            //ignore pre-existing liquids
+            floor = Blocks.water;
+        });
+
+        //blend(agaryteStone, Blocks.water, 6);
+        median(5, 0.4);
+        terrain(Blocks.carbonWall,69.86f,0.35f,1.1f);
         blend(legartyteStone, darkLegartyteStone, 6);
+
         float length = width/2.6f;
         Vec2 trns = Tmp.v1.trns(rand.random(360f), length);
         int spawnX = (int)(trns.x + width/2f), spawnY = (int)(trns.y + height/2f), endX = (int)(-trns.x + width/2f), endY = (int)(-trns.y + height/2f);
         float maxd = Mathf.dst(width/2f, height/2f);
 
         erase(spawnX, spawnY, 15);
-        brush(pathfind(spawnX, spawnY, endX, endY, tile -> (tile.solid() ? 300f : 0f) + maxd - tile.dst(width/2f, height/2f)/10f, Astar.manhattan), 9);
+        Seq<Tile> path = pathfind(spawnX, spawnY, endX, endY, tile -> (tile.solid() ? 300f : 0f) + maxd - tile.dst(width/2f, height/2f)/10f, Astar.manhattan);
+        brush(path, 13);
+        brushWithBlock(path,9, Blocks.water);
         erase(endX, endY, 15);
 
-        //TODO terrain(Blocks.carbonWall,69.86,0.35,);
-        noise(legartyteStone,agaryteWall,3,0.5f,87.3f,0.59f);
-        //TODO other stuff nahuy
-        /*
-        [
-        {class:terrain,seed:21548882,scl:69.86,threshold:0.79999995,
-            octaves:1.485,falloff:0.49499997,magnitude:0.35999998,circleScl:1.545,tilt:-0.84,block:carbon-wall},
-        {class:noise,seed:599460571,scl:87.325,threshold:0.59,octaves:3.015,
-            floor:oceanic_dust-legartyte-stone,block:oceanic_dust-agaryte-wall},
-        {class:riverNoise,seed:277412702,scl:82.335,threshold2:0.099999994,
-            floor2:darksand-water,block:oceanic_dust-agaryte-wall}]
-         */
+        blend(Blocks.water, Blocks.darksandWater, 4);
 
-        Schematics.placeLaunchLoadout(width / 2, height / 2);
+        distort(10f, 16f);
+
+        Seq<Block> ores = getOres();
+        FloatSeq frequencies = new FloatSeq();
+        float poles = Math.abs(sector.tile.v.y);
+        for(int i = 0; i < ores.size; i++){
+            frequencies.add(rand.random(-0.1f, 0.01f) + i * 0.01f + poles * 0.04f);
+        }
+        pass((x,y) -> {
+            int offsetX = x - 4, offsetY = y + 23;
+            if(Mathf.within(x-spawnX,y-spawnY,6) && rand.chance(0.3)) {
+                ore = oreSpaclanium;
+                return;
+            }
+            for(int i = ores.size - 1; i >= 0; i--){
+                Block entry = ores.get(i);
+                float freq = frequencies.get(i);
+                if(Math.abs(0.5f - noise(offsetX, offsetY + i*888, 2, 0.7, (10 + i * 2))) > 0.24f + i*0.01 &&
+                        Math.abs(0.5f - noise(offsetX, offsetY - i*888, 1, 1, (15 + i * 4))) > 0.37f + freq){
+                    ore = entry;
+                    break;
+                }
+            }
+        });
+
+
+
+        Schematics.placeLaunchLoadout(spawnX, spawnY);
+        tiles.get(endX,endY).setOverlay(Blocks.spawn);
+    }
+
+
+    public void brushWithBlock(Seq<Tile> path, int rad, Block block){
+        path.each(tile -> eraseWithBlock(tile.x, tile.y, rad,block));
+    }
+
+    public void eraseWithBlock(int cx, int cy, int rad, Block block){
+        for(int x = -rad; x <= rad; x++){
+            for(int y = -rad; y <= rad; y++){
+                int wx = cx + x, wy = cy + y;
+                if(Structs.inBounds(wx, wy, width, height) && Mathf.within(x, y, rad)){
+                    Tile other = tiles.getn(wx, wy);
+                    if(block == Blocks.water) other.setFloor(block.asFloor());
+                    else other.setBlock(block);
+                }
+            }
+        }
     }
 
     float rawHeight(Vec3 position){
@@ -155,6 +252,10 @@ public class AtlacianPlanetGenerator extends PlanetGenerator {
         float temp = Simplex.noise3d(seed, 8, 0.6, 1f/2f, position.x, position.y + 99f, position.z);
         height *= 1.2f;
         height = Mathf.clamp(height);
-        return arr[Mathf.clamp((int)(temp * arr.length), 0, arr[0].length - 1)][Mathf.clamp((int)(height * arr[0].length), 0, arr[0].length - 1)];
+        Block block = arr[Mathf.clamp((int)(temp * arr.length), 0, arr[0].length - 1)][Mathf.clamp((int)(height * arr[0].length), 0, arr[0].length - 1)];
+        if(block == Blocks.sandWall) block = legartyteWall;
+        float thirdNoise = noise(position.x,position.y,2,0.3f,10f,0.12f);
+        if(thirdNoise > liqThresh) block = Blocks.water;
+        return block;
     }
 }
