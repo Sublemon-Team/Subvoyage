7e82b21fc2389bc64b62d23a5f79acc8e1102bf9
Skoow <89569841+VuzZis@users.noreply.github.com>
2024-06-25 21:45:55 +0500

    weaver additions

--- src/subvoyage/content/world/draw/DrawColorWeave.java
+++ src/subvoyage/content/world/draw/DrawColorWeave.java
@@ -8,18 +8,22 @@ import mindustry.gen.*;
 import mindustry.world.*;
 import mindustry.world.draw.*;
 
+import java.util.HashMap;
+
 public class DrawColorWeave extends DrawBlock{
     public TextureRegion weave;
     public Color color = Color.valueOf("ffd37f");
+    public HashMap<Building,Float> degrees = new HashMap<>();
 
     public DrawColorWeave(Color color){
         this.color = color;
     }
 
     public void draw(Building build){
-        Draw.rect(this.weave, build.x, build.y, build.totalProgress());
+        Draw.rect(this.weave, build.x+Mathf.sinDeg(build.totalProgress()*8), build.y, build.totalProgress());
         Draw.color(color);
         Draw.alpha(build.warmup());
+        Lines.stroke(2f);
         Lines.lineAngleCenter(build.x + Mathf.sin(build.totalProgress(), 6.0F, 2.6666667F * (float)build.block.size), build.y, 90.0F, (float)(build.block.size * 8) / 2.0F);
         Draw.reset();
     }
