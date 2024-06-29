43e16d16fb9c2d774e499e580fd7e9449808b732
Skoow <89569841+VuzZis@users.noreply.github.com>
2024-06-29 11:18:50 +0500

    Update VaporControl.java

--- src/subvoyage/content/blocks/editor/vapor/VaporControl.java
+++ src/subvoyage/content/blocks/editor/vapor/VaporControl.java
@@ -2,13 +2,29 @@ package subvoyage.content.blocks.editor.vapor;
 
 import arc.struct.Seq;
 import mindustry.ai.Pathfinder;
+import mindustry.game.FogControl;
+import mindustry.io.SaveFileReader;
 
-public class VaporControl {
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class VaporControl implements SaveFileReader.CustomChunk {
     public static void load() {
         Seq<Pathfinder.PathCost> newCosts = Seq.with();
         Pathfinder.costTypes.forEach(e -> {
             //TODO: replace when vapor check is done
             newCosts.add((team, tile) -> false ? -1 : e.getCost(team,tile));
         });
+    }
+
+    @Override
+    public void write(DataOutput stream) throws IOException {
+
+    }
+
+    @Override
+    public void read(DataInput stream) throws IOException {
+
     }
 }
