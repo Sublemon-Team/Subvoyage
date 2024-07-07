31a9f1ad0695fee74e53fa4f8fb83436d1721992
Skoow <89569841+VuzZis@users.noreply.github.com>
2024-07-07 13:18:44 +0500

    heeeaaaat

--- src/subvoyage/content/unit/type/AtlacianUnitType.java
+++ src/subvoyage/content/unit/type/AtlacianUnitType.java
@@ -1,6 +1,12 @@
 package subvoyage.content.unit.type;
 
+import arc.Core;
+import arc.graphics.Blending;
+import arc.graphics.Color;
 import arc.graphics.g2d.Draw;
+import arc.graphics.g2d.TextureRegion;
+import mindustry.content.UnitTypes;
+import mindustry.entities.units.WeaponMount;
 import mindustry.gen.Unit;
 import mindustry.graphics.*;
 import mindustry.type.*;
@@ -10,6 +16,9 @@ import subvoyage.content.world.items.*;
 
 public class AtlacianUnitType extends UnitType{
     public float bodyScale = 1f;
+    public boolean bodyHeat = false;
+    public TextureRegion regionHeat;
+    public Color heatColor = Pal.turretHeat;
     public AtlacianUnitType(String name){
         super(name);
         outlineColor = Pal.darkOutline;
@@ -19,10 +28,37 @@ public class AtlacianUnitType extends UnitType{
     }
 
     @Override
+    public void load() {
+        super.load();
+        if(bodyHeat) regionHeat = Core.atlas.find(name+"-heat");
+    }
+
+    @Override
+    public void update(Unit unit) {
+        super.update(unit);
+        if(bodyHeat)
+            for (Weapon weapon : unit.type.weapons) {
+                weapon.heatColor = this.heatColor;
+            }
+    }
+
+    @Override
     public void drawBody(Unit unit) {
         float scl = Draw.scl;
         Draw.scl(bodyScale);
         super.drawBody(unit);
+        if(regionHeat != null && regionHeat.found()) {
+            float warmup = 0f;
+            for (WeaponMount mount : unit.mounts) {
+                warmup = Math.max(warmup, mount.warmup);
+                mount.heat = mount.warmup;
+            }
+            Draw.blend(Blending.additive);
+            Draw.color(heatColor,warmup);
+            Draw.scl(bodyScale);
+            Draw.rect(regionHeat, unit.x, unit.y, unit.rotation - 90);
+            Draw.blend();
+        }
         Draw.scl();
     }
 
