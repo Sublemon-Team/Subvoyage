dba1dc074d8814a388f52f351b9980544a5454bd
Skoow <89569841+VuzZis@users.noreply.github.com>
2024-07-09 20:20:21 +0500

    rebalanced pumps

--- src/subvoyage/content/blocks/SvBlocks.java
+++ src/subvoyage/content/blocks/SvBlocks.java
@@ -1023,7 +1023,7 @@ public class SvBlocks{
 
             squareSprite = false;
             envDisabled |= Env.scorching;
-            pumpAmount = 8f / 60f;
+            pumpAmount = 15f / 60f;
 
             researchCost = with(spaclanium,5);
         }};
@@ -1037,7 +1037,7 @@ public class SvBlocks{
 
             squareSprite = false;
             envDisabled |= Env.scorching;
-            pumpAmount = 16f / 60f;
+            pumpAmount = 75f / 60f;
             consumePower(0.35f);
 
             drawer = new DrawMulti(
