9174557c33174c6a25965572eca226bc76da74eb
Skoow <89569841+VuzZis@users.noreply.github.com>
2024-08-04 11:54:39 +0500

    Update SvMath.java

--- src/subvoyage/utility/SvMath.java
+++ src/subvoyage/utility/SvMath.java
@@ -3,15 +3,6 @@ package subvoyage.utility;
 import mindustry.gen.Building;
 
 public class SvMath {
-    /**
-     * @param in [0,1]
-     * @return out [-1,1]
-     */
-    public static float linSin(float in) {
-        in %= 1f;
-        return (in > 0.5f ? 1f-in : in)*4f-1f;
-    }
-
     public static boolean withinSquare(Building building, Building origin, float radius) {
         return withinSquare(origin.x,origin.y,building.x,building.y,radius);
     };
