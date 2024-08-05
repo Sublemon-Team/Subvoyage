e6e5fed26be0d1d984b7296b6291207376b415de
Skoow <89569841+VuzZis@users.noreply.github.com>
2024-08-05 15:42:58 +0500

    arr

--- src/subvoyage/utility/SvCall.java
+++ src/subvoyage/utility/SvCall.java
@@ -1,9 +1,8 @@
 package subvoyage.utility;
 
 import mindustry.Vars;
-import mindustry.gen.AssemblerDroneSpawnedCallPacket;
-import mindustry.gen.AssemblerUnitSpawnedCallPacket;
-import mindustry.gen.UnitBlockSpawnCallPacket;
+import mindustry.gen.*;
+import mindustry.net.Net;
 import mindustry.world.Tile;
 import mindustry.world.blocks.units.UnitAssembler;
 import mindustry.world.blocks.units.UnitBlock;
@@ -14,6 +13,14 @@ import subvoyage.type.block.laser_blocks.unit.LaserUnitAssembler;
 import subvoyage.type.block.laser_blocks.unit.LaserUnitBlock;
 
 public class SvCall {
+    static {
+        registerPackets();
+    }
+    public static void registerPackets() {
+        Net.registerPacket(LaserAssemblerDroneSpawnedCallPacket::new);
+        Net.registerPacket(LaserAssemblerUnitSpawnedCallPacket::new);
+        Net.registerPacket(LaserUnitBlockSpawnCallPacket::new);
+    }
     public static void assemblerDroneSpawned(Tile tile, int id) {
         if (Vars.net.server() || !Vars.net.active()) {
             LaserUnitAssembler.assemblerDroneSpawned(tile, id);
